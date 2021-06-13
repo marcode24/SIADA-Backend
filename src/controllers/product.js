a42e9a8952e67f9e88c3355fb9243983199aef8e
marcode24 <marco24cruz08@gmail.com>
2021-06-13 16:47:03 -0500

    edit products added

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -21,7 +21,7 @@ const createProduct = async(req = request, res = response) => {
         }
         const newProduct = new Product(req.body);
         await newProduct.save();
-        res.json({ ok: true, msg: 'product created correctly' });
+        res.json({ msg: 'product created correctly' });
     } catch (error) {
         console.log(error);
         res.status(500).json({ msg: 'something went wrong' });
@@ -74,9 +74,38 @@ const disableProduct = async(req = request, res = response) => {
     }
 }
 
+const editProduct = async(req = request, res = response) => {
+    try {
+        const regExpCode = /^[0-9]{13}$/,
+            regExpName = /^[a-zA-Z\s\S]{5,40}$/ ;
+        const { productCode, productName, stock } = req.body;
+        if(!regExpCode.test(productCode.toString())) {
+            return res.status(400).json({msg: 'product code invalid' })
+        }
+        const codeExist = await Product.findOne({where: {productCode}});
+        if(codeExist) {
+            return res.status(400).json({ msg: 'code already exists' });
+        }
+        if(!regExpName.test(productName)) {
+            return res.status(400).json({ msg: 'product name invalid' })
+        }
+        if(typeof stock !== 'number' || stock <= 0 || stock >= 1000) {
+            return res.status(400).json({ msg: 'stock invalid' })
+        }
+        const { idProduct } = req.params;
+        const productDB = await Product.findOne({where: {productCode: Number(idProduct)}})
+        await productDB.update(req.body);
+        res.json({ msg: 'product edited correctly' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'something went wrong' });
+    }
+}
+
 module.exports = {
     createProduct,
     getProducts,
     enableProduct,
-    disableProduct
+    disableProduct,
+    editProduct
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { createProduct, getProducts, enableProduct, disableProduct }  = require('../controllers/product');
+const { createProduct, getProducts, enableProduct, disableProduct, editProduct }  = require('../controllers/product');
 const router = Router();
 
 router.get('/', getProducts);
 
 router.post('/newProduct', createProduct);
-router.post('/enableProduct/:idProduct', enableProduct)
-router.post('/disableProduct/:idProduct', disableProduct)
+router.post('/enableProduct/:idProduct', enableProduct);
+router.post('/disableProduct/:idProduct', disableProduct);
+router.post('/editProduct/:idProduct', editProduct);
 
 module.exports = router;
